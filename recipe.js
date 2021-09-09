e16219bd56ab911131dac512a0d38a80ba66b241
Rania Jeanbeyh <rani0511@stud.kea.dk>
2021-09-09 12:17:36 +0200

    editing-js

--- recipe.js
+++ recipe.js
@@ -3,44 +3,44 @@ const url = "https://kea2021-e819.restdb.io/rest/cake-baking?max=4";
 
 // THE API key
 const options = {
-    headers: {
-        "x-apikey": "6137297443cedb6d1f97eda8",
-    },
+  headers: {
+    "x-apikey": "6137297443cedb6d1f97eda8",
+  },
 };
 
 fetch(url, options)
-    .then((response) => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response.json();
-    })
-    .then((data) => {
-        // console.log(data);
-        handleData(data);
-    })
-    .catch((e) => {
-        console.error("An error occured:", e.message);
-    });
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // console.log(data);
+    handleData(data);
+  })
+  .catch((e) => {
+    console.error("An error occured:", e.message);
+  });
 
 function handleData(cakes) {
-    cakes.forEach((cake) => {
-        console.log(cake);
-        // make a template
-        // grab it
-        const myTemplate = document.querySelector("template").content;
-        // clone it
-        const myClone = myTemplate.cloneNode(true);
-        // populate with data
-        myClone.querySelector("h2").textContent = cake.name;
-        myClone.querySelector("img").src = cake.image;
-        myClone.querySelector("p.ingredients").innerHTML = cake.ingredients;
-        myClone.querySelector("p.instructions").innerHTML = cake.instructions;
-        myClone.querySelector("a").textContent = cake.source;
-        // myClone.querySelector("p.toppings").innerHTML = cake.toppingTechniques;
-        // myClone.querySelector("p.toppingsIng").innerHTML = cake.toppingsIngredients;
-        // myClone.querySelector("p.utilities").innerHTML = cake.utilities;
-        // append to DOM
-        document.querySelector("main").appendChild(myClone);
-    });
+  cakes.forEach((cake) => {
+    console.log(cake);
+    // make a template
+    // grab it
+    const myTemplate = document.querySelector("template").content;
+    // clone it
+    const myClone = myTemplate.cloneNode(true);
+    // populate with data
+    myClone.querySelector("h2").textContent = cake.name;
+    myClone.querySelector("img").src = cake.image;
+    myClone.querySelector("p.materials").innerHTML = cake.ingredients;
+    myClone.querySelector("p.instructions").innerHTML = cake.instructions;
+    myClone.querySelector("a").textContent = cake.source;
+    // myClone.querySelector("p.toppings").innerHTML = cake.toppingTechniques;
+    // myClone.querySelector("p.toppingsIng").innerHTML = cake.toppingsIngredients;
+    // myClone.querySelector("p.utilities").innerHTML = cake.utilities;
+    // append to DOM
+    document.querySelector("main").appendChild(myClone);
+  });
 }
